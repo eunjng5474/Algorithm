8d5f5d4eb7ca9234cbe5e2b7df05e3373a4636a8
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-01-25 20:27:26 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,9 +1,6 @@
 x_list = []
 for i in range(28):
     x_list.append(int(input()))
-# print(x_list)
-x_list = sorted(x_list)
-# print(x_list)
 for j in range(1, 31):
     if j not in x_list:
         print(j)
