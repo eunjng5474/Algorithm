bdf243e773f1512b32a31bd2566f226f25e3f482
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-02-18 17:23:07 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,24 @@
+stack = []
+def DFS_S(start):
+    stack.append(start)    
+    visited[start] = 1
+    while stack:
+        for i in arr[start]:
+            if visited[i] == 0:
+                stack.append(i)
+                visited[i] = 1
+        start = stack.pop()
+
 N = int(input())
-M = int(input())
-
+P = int(input())
+arr = [[] * (N+1) for _ in range(N+1)]
 visited = [0] * (N+1)
-arr = [[0] * (N+1) for _ in range(N+1)]
-stack = [1]
 
-for m in range(M):
+for p in range(P):
     a, b = map(int, input().split())
-    arr[a][b] = 1
-    arr[b][a] = 1
+    arr[a].append(b)
+    arr[b].append(a)
 
-while stack:
-    start = stack.pop()
-
-    if visited[start] == 0:
-        visited[start] = 1
-        
-    for next in range(1, N+1):
-        if arr[start][next] == 1 and visited[next] == 0:
-            visited[next] = 1
-            stack.append(next)
-    
-print(visited.count(1) - 1)
+DFS_S(1)
+result_s = visited.count(1) - 1
+print(result_s)
