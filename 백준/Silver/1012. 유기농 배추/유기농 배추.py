aa1856db79db654b6cfd7529e69117ddae176e29
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-02-19 15:00:08 +0900

    [Silver II] Title: 유기농 배추, Time: 644 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def DFS(x, y):
+    arr[x][y] = 0
+
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        if 0 <= nx < M and 0 <= ny < N:
+            if arr[nx][ny] == 1:        
+                # arr[nx][ny] = 0
+                DFS(nx, ny)
+
+    
+T = int(input())
+for tc in range(T):
+    M, N, K = map(int, input().split())
+    arr = [[0] * N for _ in range(M)]
+    result = 0
+
+    for k in range(K):
+        a, b = map(int, input().split())
+        arr[a][b] = 1
+
+
+    cnt = 0
+    for x in range(M):
+        for y in range(N):
+            if arr[x][y] == 1:
+                cnt += 1
+                DFS(x, y)
+
+    print(cnt)   
