8b24d0799e74337175b54281dc79755604b7ee06
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-03-03 21:10:47 +0900

    [Silver IV] Title: 개미, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
+
+tp = (p + t) // w
+tq = (q + t) // h
+
+if not tp % 2:
+    x = (p+t) % w
+else:
+    x = w - ((p+t) % w)
+
+if not tq % 2:
+    y = (q+t) % h
+else:
+    y = h - ((q+t) % h)
+
+print(x, y)
