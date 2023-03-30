b0bc80933ac5b3e01f27ce3590f700c8652089fd
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-03-30 20:25:59 +0900

    [Silver II] Title: 스타트와 링크, Time: 3156 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,49 @@
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+people = [i for i in range(N)]
+visited = [0] * N
+start_lst = []
+result = 100 * N * N
+
+def start_sum(lst):
+    start = 0
+    for i in range(len(lst)-1):
+        for j in range(i+1, len(lst)):
+            start += arr[lst[i]][lst[j]]
+            start += arr[lst[j]][lst[i]]
+
+    return start
+
+def link_sum(lst):
+    link = 0
+    for i in range(len(lst)-1):
+        for j in range(i+1, len(lst)):
+            link += arr[lst[i]][lst[j]]
+            link += arr[lst[j]][lst[i]]
+
+    return link
+
+
+def sol(n):
+    global result
+    if len(start_lst) == N//2:
+        link_lst = []
+        start = start_sum(start_lst)
+
+        for p in people:
+            if p not in start_lst:
+                link_lst.append(p)
+        link = link_sum(link_lst)
+
+        if abs(start - link) < result:
+            result = abs(start - link)
+
+        return
+
+    for i in range(n, N):
+        start_lst.append(i)
+        sol(i+1)
+        start_lst.pop()
+
+sol(0)
+print(result)
