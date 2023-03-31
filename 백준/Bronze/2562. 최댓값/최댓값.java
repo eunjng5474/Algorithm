e89d966a54653c8deb5679d156e2e269240cd738
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-03-31 09:36:32 +0900

    [Bronze III] Title: 최댓값, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int max_num = 0;
+		int max_idx = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			int x = sc.nextInt();
+			
+			if(x > max_num) {
+				max_num = x;
+				max_idx = i+1;
+			}
+		}
+		
+		System.out.println(max_num);
+		System.out.println(max_idx);
+
+	}
+
+}
