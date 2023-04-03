f69145f4595ddf5cb7255339aa29307c3753e81d
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-04-03 20:37:11 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 340 ms, Memory: 19288 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int arr[] = new int[] {25, 10, 5, 1};
+
+		
+		for(int t=0; t<T; t++) {
+			int money = sc.nextInt();
+			
+			int result[] = new int[4];
+			for(int i=0; i<4; i++) {
+				result[i] = money / arr[i];
+				money %= arr[i];
+			}
+			
+			for(int n: result) System.out.print(n+" ");
+			System.out.println();
+		}
+		
+	}
+
+}
