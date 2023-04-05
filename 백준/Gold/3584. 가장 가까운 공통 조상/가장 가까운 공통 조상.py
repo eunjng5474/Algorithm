715dfa3d68a1fc51de09dc1d20d49b61ea55a411
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-04-05 20:49:22 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -4,20 +4,16 @@ input = sys.stdin.readline
 T = int(input())
 for tc in range(T):
     N = int(input())
-
-    child = [[] for _ in range(N+1)]
     parent = [0] * (N+1)
 
     for n in range(N-1):
         a, b = map(int, input().split())
-        child[a].append(b)
         parent[b] = a
 
     n1, n2 = map(int, input().split())
 
     lst = []
-    i = n1
-    j = n2
+    i, j = n1, n2
     while True:
         if i == 0:
             break
