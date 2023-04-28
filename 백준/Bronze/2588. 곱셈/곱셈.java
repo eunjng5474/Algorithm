09e6ee16a0f012f1ec9aae8204e640d7a489250d
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-04-28 21:29:08 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		String b = br.readLine();
+		int num_b = Integer.parseInt(b);
+		
+		System.out.println(a * (b.charAt(2) - '0'));
+		System.out.println(a * (b.charAt(1) - '0'));
+		System.out.println(a * (b.charAt(0) - '0'));
+		System.out.println(a * num_b);
+
+	}
+
+}
