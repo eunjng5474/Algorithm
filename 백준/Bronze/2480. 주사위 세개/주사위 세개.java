c5367aab8c9441e2027893798bc42a7ecd310c4d
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-05-01 22:28:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,33 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		
-		if(a == b && b == c && c == a) {
-			System.out.println(10000+a*1000); 
-		}
-		else if(a == b || b == c ) {
+		if (a==b && b==c && c==a) {
+			System.out.println(10000+a*1000);
+		} else if (a==b || b==c) {
 			System.out.println(1000+b*100);
-		}
-		else if(a == c) {
+		} else if (a==c) {
 			System.out.println(1000+a*100);
-		}
-		else {
-			int arr[] = new int[3];
+		} else {
+			int[] arr = new int[3];
 			arr[0] = a;
 			arr[1] = b;
 			arr[2] = c;
-			
 			Arrays.sort(arr);
 			System.out.println(arr[2]*100);
-
 		}
 
 	}
