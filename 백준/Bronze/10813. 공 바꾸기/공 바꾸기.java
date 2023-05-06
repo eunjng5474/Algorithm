422e160af89c54d3a88650139857b3c46afcccb1
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-05-06 21:53:59 +0900

    [Bronze II] Title: 공 바꾸기, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,30 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
-		int arr[] = new int[N+1];
-		for(int i=0; i<N+1; i++) {
+		int arr[] = new int[n+1];
+		for(int i=0; i<n+1; i++) {
 			arr[i] = i;
 		}
 		
-		for(int m=0; m<M; m++) {
-			int i = sc.nextInt();
-			int j = sc.nextInt();
+		for(int k=0; k<m; k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
 			
 			int tmp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = tmp;
 		}
-		
-		for(int i=1; i<N+1; i++) {
-			System.out.print(arr[i]+" ");
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=n; i++) {
+			sb.append(arr[i]);
+			sb.append(" ");
 		}
-
+		System.out.println(sb);
 	}
 
 }
