08d1dc074eab6d30f67c5759e0048e975eafc74b
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-05-06 20:34:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 596 ms, Memory: 89928 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,25 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	//////// sol 2
+	////////// sol 1
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+		
 		int n = Integer.parseInt(br.readLine());
-		int[] lst = new int[n];
-		int idx = 0;
+		int max = - 1000001;
+		int min = 1000001;
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		while(st.hasMoreTokens()) {
-			lst[idx] = Integer.parseInt(st.nextToken());
-			idx++;
+			int num = Integer.parseInt(st.nextToken());
+			if(num > max) {
+				max = num;
+			}
+			if(num < min) {
+				min = num;
+			}
 		}
-		Arrays.sort(lst);
-		System.out.println(lst[0] + " " + lst[n-1]);
+		System.out.println(min + " " + max);
 	}
+	
+	
+	
+//	//////// sol 2
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int n = Integer.parseInt(br.readLine());
+//		int[] lst = new int[n];
+//		int idx = 0;
+//		
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		while(st.hasMoreTokens()) {
+//			lst[idx] = Integer.parseInt(st.nextToken());
+//			idx++;
+//		}
+//		
+//
+//		Arrays.sort(lst);
+//		System.out.println(lst[0] + " " + lst[n-1]);
+//	}
 
 }
