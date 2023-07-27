fe054a894d5f0a6e4bae9c4fbc88a2c34aaf3094
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-07-27 21:07:08 +0900

    [Bronze III] Title: 직각삼각형, Time: 116 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,32 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
 		while(true) {
-			int arr[] = new int[3];
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
+			int[] nums = new int[3];
 			for(int i=0; i<3; i++) {
-				arr[i] = sc.nextInt();
+				nums[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			if(arr[0]==0 && arr[1]==0 && arr[2]==0) break;
-
-			Arrays.sort(arr);
+			if(nums[0] == 0 && nums[1] == 0 && nums[2] == 0) break;
 			
-			if(arr[0]*arr[0] + arr[1]*arr[1] == arr[2]*arr[2]) {
-				System.out.println("right");
+			Arrays.sort(nums);
+			
+			if(nums[2]*nums[2] == nums[1]*nums[1] + nums[0]*nums[0]) {
+				sb.append("right").append("\n");
+			} else {
+				sb.append("wrong").append("\n");
 			}
-			else {
-				System.out.println("wrong");
-			}
-
-			
-			
 		}
+		System.out.println(sb);
+		
 
 	}
 
