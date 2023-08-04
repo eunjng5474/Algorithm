bb21049db4d2a2aad312da763bef1d634a634c7f
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-08-04 20:42:58 +0900

    [Bronze II] Title: 분해합, Time: 140 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -6,25 +6,23 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
+
+		int n = Integer.parseInt(br.readLine());
 		int result = 0;
 		
-		for(int i=0; i<N; i++) {
-			int num = i;
+		for(int i=0; i<n; i++) {
 			int sum = i;
+			int num = i;
 			
 			while(num != 0) {
 				sum += num % 10;
 				num /= 10;
 			}
-			
-			if(sum == N) {
+			if(sum == n) {
 				result = i;
 				break;
 			}
 		}
-		
 		System.out.println(result);
 
 	}
