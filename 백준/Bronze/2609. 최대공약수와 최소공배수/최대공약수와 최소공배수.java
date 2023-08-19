7aaf0a1129f11ba38a326854573218fa5fc1e3fa
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-08-19 22:36:39 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 132 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int c = sol(a, b);
+		
+		System.out.println(c);
+		System.out.println(a * b / c);
+		
+
+	}
+	
+	public static int sol(int a, int b) {
+		if (b == 0) return a;
+		
+		return sol(b, a % b);
+	}
+
+}
