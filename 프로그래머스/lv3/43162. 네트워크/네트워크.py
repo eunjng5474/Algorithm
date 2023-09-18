00c57031a03947fa5af6980394e73cd1405c2dac
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-09-18 21:26:00 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,17 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    def dfs(i):
+        visited[i] = True
+        
+        for j in range(n):
+            if computers[i][j] and not visited[j]:
+                dfs(j)
+    
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+    
+    return answer
