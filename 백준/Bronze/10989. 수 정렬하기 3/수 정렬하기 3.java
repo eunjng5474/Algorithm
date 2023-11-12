6fa104e536e7548ba352f43fd0d9c1e6188a93c6
eunjng5474 <122499795+eunjng5474@users.noreply.github.com>
2023-11-12 20:52:30 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2580 ms, Memory: 362520 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,29 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] cnt = new int[10001];
-		
-		for(int i=0; i<n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			cnt[num] += 1;
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=1; i<10001; i++) {
-			while(cnt[i] > 0) {
-				sb.append(i);
-				sb.append('\n');
-				cnt[i] -= 1;
-			}
-		}
-		System.out.println(sb);
-	}
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
