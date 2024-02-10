5f75d2f356c9c0876dd7b57d14dc5bab3eca517e
곽은정 <eunjng5474@gmail.com>
2024-02-10 20:28:26 +0900

    [Silver II] Title: 유기농 배추, Time: 168 ms, Memory: 15776 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int m, n, k;
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            arr = new int[n][m];
+            int cnt = 0;
+
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[y][x] = 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (arr[i][j] == 1) {
+                        dfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+            sb.append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int x, int y) {
+        arr[x][y] = -1;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if (arr[nx][ny] == 1) {
+                arr[nx][ny] = -1;
+                dfs(nx, ny);
+            }
+        }
+    }
+}
