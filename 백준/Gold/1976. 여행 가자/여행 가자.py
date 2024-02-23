c57e65ff5f20039d73eaa77a4f5af02cb8b9d4d7
곽은정 <eunjng5474@gmail.com>
2024-02-23 21:27:03 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,28 +1,37 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def bfs(start):
-    q = deque()
-    q.append(start)
-    visited[start] = True
-    while q:
-        now = q.pop()
-        for i in range(n):
-            if arr[now][i] and not visited[i]:
-                visited[i] = True
-                q.append(i)
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x > y:   # x의 부모보다 y의 부모가 상위 루트
+        parent[x] = y   # x의 부모를 y의 부모로
+    else:
+        parent[y] = x
+
+def find(x):
+    if x == parent[x]:  # x가 부모노드이면 x 반환
+        return x
+    parent[x] = find(parent[x])     # 부모노드 탐색
+    return parent[x]
 
 n = int(input())
 m = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-plan = list(map(int, input().split()))
+parent = [i for i in range(n)]
 
-visited = [False] * (n+1)
+for i in range(n):
+    arr = list(map(int, input().split()))
+    for j in range(n):
+        if arr[j]:  # i, j 연결되어 있으면 union
+            union(i, j)
+
+plan = list(map(int, input().split()))
 result = "YES"
-bfs(plan[0] - 1)
-for p in plan:
-    if not visited[p-1]:
+for i in range(1, m):
+    if parent[plan[i]-1] != parent[plan[0]-1]:
+        # plan 중에서 부모노드가 계획의 첫번째 도시의 부모와 다른 경우 있으면 불가능
         result = "NO"
         break
+
 print(result)
