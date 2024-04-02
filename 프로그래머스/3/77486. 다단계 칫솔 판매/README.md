1c66dc592c9f9ab7f288946f0f0e30780b543037
곽은정 <eunjng5474@gmail.com>
2024-04-02 16:33:07 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 129.76 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486) 
 
 ### 성능 요약
 
-메모리: 20.8 MB, 시간: 142.42 ms
+메모리: 20.8 MB, 시간: 129.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 16:21:53
+2024년 04월 02일 16:33:04
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,6 +1,9 @@
 def solution(enroll, referral, seller, amount):
     n = len(enroll)
     answer = [0 for _ in range(n)]
+    
+    # list.index() 사용 시 시간초과 (시간복잡도: O(n))
+    # -> index라는 dictionary 만들어서 인덱스 저장 
     index = {}
     for i in range(n):
         index[enroll[i]] = i
@@ -9,6 +12,9 @@ def solution(enroll, referral, seller, amount):
         cal = money // 10
         i = index[name]
         answer[i] += (money - cal)
+        
+        # 1원 미만이라서 분배할 이득이 없거나
+        # 추천인 없으면 return 
         if cal < 1:
             return
         if referral[i] == "-":
