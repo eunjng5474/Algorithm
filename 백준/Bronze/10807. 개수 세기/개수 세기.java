f4391e0c0c5bbb313ea7fca42ce507bae0a0a60d
곽은정 <eunjng5474@gmail.com>
2024-06-01 16:34:54 +0900

    [Bronze V] Title: 개수 세기, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int v = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            if(nums[i] == v) result++;
+        }
+        System.out.println(result);
+    }
+}
